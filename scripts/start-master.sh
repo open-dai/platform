e06f806e2bb502b373a5747cf10984bbe974a827
gioppoluca <gioppoluca@libero.it>
2014-06-20 17:12:16 +0200

    Added puppet, augeas and php

--- scripts/start-master.sh
+++ scripts/start-master.sh
@@ -39,7 +39,7 @@ function start-opendai {
 	log "adding repos"
 	#add puppet repository
 	rpm --import https://fedoraproject.org/static/0608B895.txt
-	rpm -ivh http://yum.puppetlabs.com/el/6/products/x86_64/puppetlabs-release-6-7.noarch.rpm
+	rpm -ivh http://yum.puppetlabs.com/el/6/products/x86_64/puppetlabs-release-6-10.noarch.rpm
 	#add RPMFORGE repository
 	rpm -ivh http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm
 	#add EPEL repository
@@ -50,8 +50,7 @@ function start-opendai {
 	
 	log "Cloudstack stuff"
 	#First cloudstack recover virtual router IP
-	server_ip=$(cat /var/lib/dhclient/dhclient-eth0.leases | grep dhcp-server-identi
-	fier | tail -1| awk '{print $NF}' | tr '\;' ' ')
+	server_ip=$(cat /var/lib/dhclient/dhclient-eth0.leases | grep dhcp-server-identifier | tail -1| awk '{print $NF}' | tr '\;' ' ')
 	server_ip2=${server_ip:0:${#server_ip}-1}
 	log "Cloudstsack virtual router" $server_ip2
 	userdata=$(curl http://$server_ip2/latest/user-data)
@@ -60,7 +59,7 @@ function start-opendai {
 	#transform userdata in env vars
 	eval $userdata
 	
-	if [[ -z "$timezione" ]]; then timezione='Rome'; fi
+	if [[ -z "$timezone" ]]; then timezone='Rome'; fi
 	
 	
 	#install ntp  
@@ -80,6 +79,54 @@ function start-opendai {
 	chkconfig --levels 235 httpd on
 	service httpd start
 	log "started httpd" $(service httpd status)
+	
+	# Postgresql
+	ensure_package_installed "postgresql-server"
+	service postgresql initdb
+	service postgresql start
+	chkconfig --levels 235 postgresql on
+	
+	# PHP
+	ensure_package_installed "php"
+
+	# Configuration tool Augeas
+	ensure_package_installed "augeas"
+	
+
+	# Puppet Master
+	ensure_package_installed "puppet-server"
+	
+	
+	#Puppet, Dashboard and MCollective
+myHostname=$(facter fqdn)
+myIP=$(facter ipaddress)
+myDomain=$(facter domain)
+puppetDB=mgmtdb.$myDomain
+mc_pwd=mcopwd
+mc_stomp_pwd=mcopwd
+dash_db_pwd=dashboard
+log "hostname" $myHostname
+log "IP" $myIP
+log "domain" $myDomain
+log "mc_pwd" $mc_pwd
+log "mc_stomp_pwd" $mc_stomp_pwd
+log "dash_db_pwd" $dash_db_pwd
+	
+	# Configuration of puppet.conf
+	augtool ins confdir before /files/etc/puppet/puppet.conf/main/logdir -s
+	augtool set /files/etc/puppet/puppet.conf/main/confdir /etc/puppet -s
+	augtool ins vardir before /files/etc/puppet/puppet.conf/main/logdir -s
+	augtool set /files/etc/puppet/puppet.conf/main/vardir /var/lib/puppet -s
+
+	res=$(augtool defnode certname /files/etc/puppet/puppet.conf/main/certname ${myHostname,,} -s)
+	log $res
+	augtool defnode certname /files/etc/puppet/puppet.conf/main/certname $(facter fqdn) -s
+	augtool defnode modulepath /files/etc/puppet/puppet.conf/master/modulepath \$confdir/environments/\$environment/modules -s
+	augtool defnode manifest /files/etc/puppet/puppet.conf/master/manifest \$confdir/environments/\$environment/manifests/unknown_environment.pp -s
+	augtool defnode hiera_config /files/etc/puppet/puppet.conf/master/hiera_config \$confdir/environments/\$environment/hiera.yaml -s
+	augtool defnode manifest /files/etc/puppet/puppet.conf/production/manifest \$confdir/environments/\$environment/manifests/site.pp -s
+	augtool defnode manifest /files/etc/puppet/puppet.conf/dev/manifest \$confdir/manifests/site.pp -s
+	
 
 	
 }
