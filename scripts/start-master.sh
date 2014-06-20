df5f070e7195c5734a76451f83334abe998d8c27
gioppoluca <gioppoluca@libero.it>
2014-06-20 12:05:30 +0200

    initial commit of new deployment strategy

---
+++ scripts/start-master.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# version: 3 date: giovedì 13. set.2012
+
+if [ -f /etc/odailock.lck ];
+  then
+    exit
+  else
+#Script variables
+LOG_FILE=/var/log/odai-startup.log
+
+# Functions useful for the script
+function log()
+{
+    message="$@"
+    echo $message
+    echo $message >> $LOG_FILE
+}
+
+log "Starting the configuration of the Open-DAI Master Machine"
+#proper script actions
+sleep 20
