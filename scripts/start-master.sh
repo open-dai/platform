0e616a66596d4e635dc45e12c0558415808c0afd
gioppoluca <gioppoluca@libero.it>
2014-06-20 14:20:54 +0200

    change of approach

--- scripts/start-master.sh
+++ scripts/start-master.sh
@@ -1,30 +1,6 @@
-#!/bin/bash
-# version: 3 date: giovedì 13. set.2012
-
-if [ -f /etc/odailock.lck ];
-  then
-    exit
-  else
-#Script variables
-LOG_FILE=/var/log/odai-startup.log
-
-# Functions useful for the script
-function log()
-{
-    message="$@"
-    echo $message
-    echo $message >> $LOG_FILE
+function start-opendai {
+	log "sono arrivato"
 }
 
-log "Starting the configuration of the Open-DAI Master Machine"
-#proper script actions
-sleep 20
-
-
-
-
-
-
-#create the lock file so this script will not be executed each time at startup
-touch /etc/odailock.lck
-fi
+#execute the tasks
+start-opendai
